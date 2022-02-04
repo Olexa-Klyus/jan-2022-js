ffaa79801a4ab533a98b58941f389f98e208fee0
Olexa-Klyus <olexakljus@gmail.com>
2022-02-04 19:24:27 +0200

    lesson2 cw 100%

--- lesson2/cw/main.js
+++ lesson2/cw/main.js
@@ -2,21 +2,20 @@
 //     Потрібно знайти та вивести максимальне число з тих двох .
 //     Також потрібно врахувати коли введені рівні числа.
 
-// {
-//     let number1 = +prompt('enter the first number', 10);
-//     let number2 = +prompt('enter the second number', 20);
-//
-//     if (number1 === number2) {
-//         document.write('<h3>first number ' + number1 + ' is equal to second number ' + number2 + '</h3>');
-//     } else if (number1 > number2) {
-//         document.write('<h3>first number ' + number1 + ' has the maximum value</h3>');
-//     } else if (number1 < number2) {
-//         document.write('<h3>second number ' + number2 + ' has the maximum value</h3>');
-//     } else {
-//         document.write('<h3>you entered an unacceptable value</h3>');
-//     }
-// }
+{
+    let number1 = +prompt('enter the first number', 10);
+    let number2 = +prompt('enter the second number', 20);
 
+    if (number1 === number2) {
+        document.write('<h3>first number ' + number1 + ' is equal to second number ' + number2 + '</h3>');
+    } else if (number1 > number2) {
+        document.write('<h3>first number ' + number1 + ' has the maximum value</h3>');
+    } else if (number1 < number2) {
+        document.write('<h3>second number ' + number2 + ' has the maximum value</h3>');
+    } else {
+        document.write('<h3>you entered an unacceptable value</h3>');
+    }
+}
 // - У першому під'їзді квартири з 1 до 20. У другому з 21 по 48. У третьому з 49 по 90.
 // Користувач вводить номер квартири просто в змінні або через prompt('') .
 //     Програма повинна зазначити, в якому під'їзді знаходиться дана квартира.
@@ -42,7 +41,7 @@
 //   якщо в змінну записали стрінгу вивести 2, якщо булеве значення - 3, якщо об'єкт або масив - 3.
 // якщо в змінну записали щось інше, спрацьовує else
 {
-    let x ;
+    let x;
     switch (typeof x) {
         case'number':
             console.log(1);
@@ -63,9 +62,31 @@
 // - Задача-гра "чи ми йдемо сьогодні в OKTEN на навчання?".
 //     Змінна, яка характеризує температуру. Якщо температура від +10 до +22 вивести повідомлення що ми йдемо ВЧИТИСЯ .
 //     Якщо інша темпаретура, тоді виводимо повідомлення що сидимо вдома і вчимося ОНЛАЙН
-
-
-
-
+{
+    let temperature = Math.floor(Math.random() * 60 - 25);   // випадкове значення в діапазоні від -25 до +35
+    console.log((temperature > 9 && temperature < 23) ? 'Температура = ' + temperature + ' -ми йдемо ВЧИТИСЯ' : 'Температура = ' + temperature + ' -сидимо вдома і вчимося ОНЛАЙН');
+}
 // - За допомогою switch case - реалізувати гру - вікторину, де ми вводимо число від 1 до 5. І взалежності від введеного
 //     числа нам дається приз. (Авто, Мото, Телефон, і т.д ) і якщо число не підходить тоді  вивести Повідомлення - що число не вірне ... .
+{
+    let number = prompt('Введіть число від 1 до 5');
+    switch (number) {
+        case '1':
+            document.write('ви виграли приз - АВТО');
+            break;
+        case '2':
+            document.write('ви виграли приз - МОТОЦИКЛ');
+            break;
+        case'3':
+            document.write('ви виграли приз - ТЕЛЕФОН');
+            break;
+        case'4':
+            document.write('ви виграли приз - ПЛАНШЕТ');
+            break;
+        case'5':
+            document.write('ви виграли приз - МОНІТОР');
+            break;
+        default:
+            document.write('ЧИСЛО НЕ ВІРНЕ');
+    }
+}
